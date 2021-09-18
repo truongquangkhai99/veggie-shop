7d71a405f7cc3910cee40730d0a6b2be97931828
Thanh Lam <62226747+ThanhLamm@users.noreply.github.com>
2021-09-18 15:20:22 +0700

    First commit

---
+++ src/main/java/com/shop/VeggieShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeggieShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeggieShopApplication.class, args);
+	}
+
+}
